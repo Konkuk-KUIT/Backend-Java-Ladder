a3f9b347f71f55b73a84a98b2a33d7e90fee7051
Mouon <ahemsapsldk@gmail.com>
2023-09-13 11:42:04 +0900

    testLadderReachesDestination()_created

--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -1,22 +1,39 @@
 package ladder;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class LadderTest {
 
+    @DisplayName("사다리 생성 확인")
     @Test
-    void 사다리_생성_확인() {
+    void ladderCreatedConfirm() {
         //given
         int numberOfRow = 3;
         int numberOfPerson = 5;
 
         //when
         Ladder ladder = new Ladder(numberOfRow, numberOfPerson);
-
         //then
         assertNotNull(ladder);
+    }
+
+    @DisplayName("사다리 도착지점 정상 반환확인")
+    @Test
+    void testLadderReachesDestination(){
+        //given
+        int numberOfRow = 3;
+        int numberOfPerson = 5;
+
+        //when
+        Ladder ladder = new Ladder(numberOfRow, numberOfPerson);
+        //then
+        int result = ladder.run(1);
+        assertThat(result).isGreaterThan(0);
+        assertThat(result).isInstanceOf(Integer.class);
+        assertThat(result).isBetween(0, numberOfPerson - 1);
     }
 
 }
