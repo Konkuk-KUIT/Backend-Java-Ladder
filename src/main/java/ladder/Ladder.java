b86aeadc74f5bee849ff0052e2f844b5798aa8d8
jaeuk520 <jaeuk520@naver.com>
2023-09-15 01:36:55 +0900

    feat: 사다리 타기 구현

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -7,4 +7,40 @@ public class Ladder {
     public Ladder(int numberOfRow, int numberOfPerson) {
         rows = new int[numberOfRow][numberOfPerson];
     }
+
+    public int[][] getRows() {
+        return rows;
+    }
+
+    public void drawLine(int row, int col) {
+        validateIndex(row, col);
+        validateLineDuplication(row, col);
+        this.rows[row][col] = 1;
+        this.rows[row][col + 1] = -1;
+    }
+
+    public int run(int idx) {
+        for (int i=0; i<rows.length; i++) {
+            idx = moveLine(i, idx);
+        }
+        return idx;
+    }
+
+    private int moveLine(int row, int col) {
+        if(rows[row][col] == 1) return col+1;
+        if(rows[row][col] == -1) return col-1;
+        return col;
+    }
+
+    private void validateIndex(int row, int col) {
+        if(row < 0 || col < 0 || row >= rows.length || col >= rows[0].length - 1) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateLineDuplication(int row, int col) {
+        if(rows[row][col] != 0 || rows[row][col + 1] != 0) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -1,7 +1,9 @@
 package ladder;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class LadderTest {
@@ -9,8 +11,8 @@ class LadderTest {
     @Test
     void 사다리_생성_확인() {
         //given
-        int numberOfRow = 3;
-        int numberOfPerson = 5;
+        int numberOfRow = 3; //사다리 높이
+        int numberOfPerson = 5; //사다리 줄 개수
 
         //when
         Ladder ladder = new Ladder(numberOfRow, numberOfPerson);
@@ -19,4 +21,84 @@ class LadderTest {
         assertNotNull(ladder);
     }
 
+    @Test
+    void 사다리_라인_생성_확인() {
+        //given
+        int numberOfRow = 3;
+        int numberOfPerson = 4;
+
+        //when
+        Ladder ladder = new Ladder(numberOfRow, numberOfPerson);
+        ladder.drawLine(0,0);
+
+        //then
+        assertEquals(1, ladder.getRows()[0][0]);
+        assertEquals(-1, ladder.getRows()[0][1]);
+    }
+
+    @Test
+    void 사다리_라인_생성_인덱스_오류() {
+
+        //given
+        int numberOfRow = 3;
+        int numberOfPerson = 4;
+
+        //when
+        Ladder ladder = new Ladder(numberOfRow, numberOfPerson);
+
+        //then
+        assertThrows(Exception.class, () -> ladder.drawLine(-1,0));
+        assertThrows(Exception.class, () -> ladder.drawLine(3,2));
+        assertThrows(Exception.class, () -> ladder.drawLine(0,-1));
+        assertThrows(Exception.class, () -> ladder.drawLine(2,4));
+    }
+
+    @Test
+    void 사다리_라인_생성_중복_오류() {
+
+        //given
+        int numberOfRow = 3;
+        int numberOfPerson = 4;
+
+        //when
+        Ladder ladder = new Ladder(numberOfRow, numberOfPerson);
+        ladder.drawLine(1,1);
+
+        //then
+        assertThrows(Exception.class, () -> ladder.drawLine(1,1));
+        assertThrows(Exception.class, () -> ladder.drawLine(1,2));
+        assertThrows(Exception.class, () -> ladder.drawLine(1,0));
+    }
+
+    @Test
+    void 사다리_실행_확인() {
+        //given
+        int numberOfRow = 3;
+        int numberOfPerson = 4;
+
+        //when
+        Ladder ladder = new Ladder(numberOfRow, numberOfPerson);
+        ladder.drawLine(0,0);
+        ladder.drawLine(1,2);
+        ladder.drawLine(2,1);
+
+        //then
+        assertEquals(2, ladder.run(0));
+        assertEquals(0, ladder.run(1));
+        assertEquals(3, ladder.run(2));
+        assertEquals(1, ladder.run(3));
+    }
+
+    @Test
+    void 사다리_실행_확인2() {
+        //given
+        int numberOfRow = 0;
+        int numberOfPerson = 2;
+
+        //when
+        Ladder ladder = new Ladder(numberOfRow, numberOfPerson);
+
+        //then
+        assertEquals(1, ladder.run(1));
+    }
 }
