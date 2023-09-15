eb16a588d5a0b5f3a2b5cc80d785307bedda92a6
DDongYul <comeme0101@naver.com>
2023-09-15 16:22:09 +0900

    코드 리팩토링, 예외처리 추가

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -3,20 +3,23 @@ package ladder;
 public class Ladder {
 
     private final int[][] rows;
-    private int numberOfRow;
-    private int numberOfPerson;
+    private final int numberOfRow;
+    private final int numberOfPerson;
 
     public Ladder(int numberOfRow, int numberOfPerson) {
-        rows = new int[numberOfRow][numberOfPerson];
+        validateNumberOfPerson(numberOfPerson);
         this.numberOfRow = numberOfRow;
         this.numberOfPerson = numberOfPerson;
+        rows = new int[numberOfRow][numberOfPerson];
     }
 
-    public int[][] getRows() {
-        return rows;
+    public int getRowsValue(int x, int y) {
+        return rows[x][y];
     }
 
     public void drawLine(int x, int y) {
+        validatePosition(x,y);
+        //(x,y)에서 오른쪽으로 한 줄 추가
         rows[x-1][y-1] = 1;
         rows[x-1][y] = -1;
     }
@@ -34,4 +37,18 @@ public class Ladder {
         return curr+1;
     }
 
+    private void validateNumberOfPerson(int numberOfPerson) {
+        if (numberOfPerson < 1) {
+            throw new RuntimeException();
+        }
+    }
+
+    private void validatePosition(int x, int y) {
+        if (x <= 0 || y <= 0 || x > numberOfRow || y > numberOfPerson-1) {
+            throw new RuntimeException();
+        } //배열에 맞지 않은 값 (y: 마지막 사람 라인에서 오른쪽으로 라인을 그을 수 없음)
+        if (getRowsValue(x-1,y-1) ==1 || getRowsValue(x-1,y-1) ==-1) {
+            throw new RuntimeException();
+        } //이미 해당 좌표에 라인이 있을 때
+    }
 }
--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -2,9 +2,9 @@ package ladder;
 
 import org.assertj.core.api.Assertions;
 import static org.assertj.core.api.Assertions.assertThat;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
 
 class LadderTest {
 
@@ -31,14 +31,59 @@ class LadderTest {
 
         //when
         ladder.drawLine(1,1);
+
         //then
-        int[][] rows = ladder.getRows();
-        assertThat(rows[0][0]).isEqualTo(1);
-        assertThat(rows[0][1]).isEqualTo(-1);
+        assertThat(ladder.getRowsValue(0,0)).isEqualTo(1);
+        assertThat(ladder.getRowsValue(0,1)).isEqualTo(-1);
     }
 
     @Test
-    void run_테스트() throws Exception{
+    void run_테스트_빈_사다리(){
+        //given
+        int numberOfRow = 3;
+        int numberOfPerson = 5;
+
+        //when
+        Ladder ladder = new Ladder(numberOfRow, numberOfPerson);
+
+        //then (빈 사다리)
+        assertThat(ladder.run(1)).isEqualTo(1);
+        assertThat(ladder.run(3)).isEqualTo(3);
+        assertThat(ladder.run(5)).isEqualTo(5);
+    }
+    @Test
+    public void run_테스트_라인_있을때(){
+        //given
+        int numberOfRow = 3;
+        int numberOfPerson = 5;
+        Ladder ladder = new Ladder(numberOfRow, numberOfPerson);
+
+        //when
+        ladder.drawLine(1,1);
+        ladder.drawLine(2, 2);
+        ladder.drawLine(2, 4);
+        ladder.drawLine(3, 4);
+
+        //then
+        assertThat(ladder.run(1)).isEqualTo(3);
+        assertThat(ladder.run(2)).isEqualTo(1);
+        assertThat(ladder.run(3)).isEqualTo(2);
+        assertThat(ladder.run(4)).isEqualTo(4);
+        assertThat(ladder.run(5)).isEqualTo(5);
+    }
+
+    @Test
+    public void 인원_예외_테스트(){
+        //given
+        int numberOfRow = 5;
+        int numberOfPerson = 0;
+        //when
+
+        //then
+        assertThrows(RuntimeException.class, () -> new Ladder(numberOfRow, numberOfPerson));
+    }
+    @Test
+    public void drawLine_예외_테스트_배열에_맞는값(){
         //given
         int numberOfRow = 3;
         int numberOfPerson = 5;
@@ -47,14 +92,26 @@ class LadderTest {
         //when
 
         //then
-        assertThat(ladder.run(1)).isEqualTo(1);
-        assertThat(ladder.run(2)).isEqualTo(2);
+        assertThrows(RuntimeException.class, () -> ladder.drawLine(2, 5));
+        assertThrows(RuntimeException.class, () -> ladder.drawLine(4, 2));
+        assertThrows(RuntimeException.class, () -> ladder.drawLine(0, 1));
+        assertThrows(RuntimeException.class, () -> ladder.drawLine(1, 0));
+//        assertThrows(RuntimeException.class, () -> ladder.drawLine(3, 4)); // 실패
+    }
+
+    @Test
+    public void drawLine_예외_테스트_이미_값이_존재(){
+        //given
+        int numberOfRow = 3;
+        int numberOfPerson = 5;
+
+        Ladder ladder = new Ladder(numberOfRow, numberOfPerson);
 
         //when
-        ladder.drawLine(1,1);
+        ladder.drawLine(1, 1);
+
         //then
-        assertThat(ladder.run(1)).isEqualTo(2);
-        assertThat(ladder.run(2)).isEqualTo(1);
+        assertThrows(RuntimeException.class, () -> ladder.drawLine(1, 2));
     }
 
 }
