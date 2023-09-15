0e126a50352f7dc2db1153a2cd6e240a05055814
DDongYul <comeme0101@naver.com>
2023-09-15 11:58:39 +0900

    객체지향을 고려하지 않은 단순 구현

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -3,8 +3,35 @@ package ladder;
 public class Ladder {
 
     private final int[][] rows;
+    private int numberOfRow;
+    private int numberOfPerson;
 
     public Ladder(int numberOfRow, int numberOfPerson) {
         rows = new int[numberOfRow][numberOfPerson];
+        this.numberOfRow = numberOfRow;
+        this.numberOfPerson = numberOfPerson;
     }
+
+    public int[][] getRows() {
+        return rows;
+    }
+
+    public void drawLine(int x, int y) {
+        rows[x-1][y-1] = 1;
+        rows[x-1][y] = -1;
+    }
+
+    public int run(int number) {
+        int curr= number-1;
+        for (int i = 0; i < numberOfRow; i++) {
+            if (rows[i][curr] == 1) {
+                curr+=1;
+            }
+            else if(rows[i][curr] == -1){
+                curr-=1;
+            }
+        }
+        return curr+1;
+    }
+
 }
--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -1,5 +1,7 @@
 package ladder;
 
+import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -17,6 +19,42 @@ class LadderTest {
 
         //then
         assertNotNull(ladder);
+    }
+
+    @Test
+    void drawLine_테스트(){
+        //given
+        int numberOfRow = 3;
+        int numberOfPerson = 5;
+
+        Ladder ladder = new Ladder(numberOfRow, numberOfPerson);
+
+        //when
+        ladder.drawLine(1,1);
+        //then
+        int[][] rows = ladder.getRows();
+        assertThat(rows[0][0]).isEqualTo(1);
+        assertThat(rows[0][1]).isEqualTo(-1);
+    }
+
+    @Test
+    void run_테스트() throws Exception{
+        //given
+        int numberOfRow = 3;
+        int numberOfPerson = 5;
+
+        Ladder ladder = new Ladder(numberOfRow, numberOfPerson);
+        //when
+
+        //then
+        assertThat(ladder.run(1)).isEqualTo(1);
+        assertThat(ladder.run(2)).isEqualTo(2);
+
+        //when
+        ladder.drawLine(1,1);
+        //then
+        assertThat(ladder.run(1)).isEqualTo(2);
+        assertThat(ladder.run(2)).isEqualTo(1);
     }
 
 }
