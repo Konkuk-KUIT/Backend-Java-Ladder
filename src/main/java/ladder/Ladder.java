d0d3fb521ddd6f20d5b515cb49f5fb13a94162de
lee-june-young <uhiccup@gmail.com>
2023-09-16 22:02:09 +0900

    Ladder init

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -7,4 +7,24 @@ public class Ladder {
     public Ladder(int numberOfRow, int numberOfPerson) {
         rows = new int[numberOfRow][numberOfPerson];
     }
+
+    public int run(int i) {
+        if (i >= rows[0].length) { //범위 체크
+            throw new RuntimeException();
+        }
+
+        for (int level=0; level< rows.length; level++){
+            i += rows[level][i-1];
+        }
+        return i;
+    }
+
+    public void drawLine(int startColumn, int row) {
+        if (startColumn >= rows[0].length-1 || row >= rows.length) { //범위 체크
+            throw new RuntimeException();
+        }
+
+        rows[startColumn-1][row-1] = 1;
+        rows[startColumn][row-1] = -1;
+    }
 }
--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -2,7 +2,7 @@ package ladder;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class LadderTest {
 
@@ -17,6 +17,49 @@ class LadderTest {
 
         //then
         assertNotNull(ladder);
+    }
+
+    @Test
+    void runTest() {
+        //given
+        int numberOfRow = 3;
+        int numberOfPerson = 5;
+
+        //when
+        Ladder ladder = new Ladder(numberOfRow, numberOfPerson);
+        int result = ladder.run(1);
+
+        //then
+        assertEquals(result, 1);
+    }
+
+
+    @Test
+    void drawLineTest() {
+        //given
+        int numberOfRow = 3;
+        int numberOfPerson = 5;
+
+        //when
+        Ladder ladder = new Ladder(numberOfRow, numberOfPerson);
+        ladder.drawLine(1, 1); //시작 사다리, 높이(위쪽이 1, 아래로 갈수록 증가)
+        int result = ladder.run(1);
+
+        //then
+        assertEquals(result, 2);
+    }
+
+    @Test
+    void drawLine_입력_오류() {
+        //given
+        int numberOfRow = 3;
+        int numberOfPerson = 5;
+
+        //when
+        Ladder ladder = new Ladder(numberOfRow, numberOfPerson);
+
+        //then
+        assertThrows(Exception.class, ()->ladder.drawLine(4, 1));
     }
 
 }
