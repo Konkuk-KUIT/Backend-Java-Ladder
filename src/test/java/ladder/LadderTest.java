e2aa3d3955310ff9d9d1d2750203aa9e3fa4fbed
rlaxoqkf <rlaxoqkf96@gmail.com>
2023-09-16 15:31:31 +0900

    테스트 코드 변경

--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -8,6 +8,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LadderTest {
 
+    Ladder ladder;
+
     @DisplayName("사다리 생성")
     @Test
     void 사다리_생성_확인() {
@@ -22,34 +24,35 @@ class LadderTest {
         assertNotNull(ladder);
     }
 
+    // 모든 테스트에서 공통적으로 사다리 생성이 되므로 이를 줄이기 위해서 BeforEach로 각 테스트 실행전에 사다리를 생성하게 함
+    @BeforeEach
+    void setup(){
+        ladder = new Ladder(3,5);
+    }
+
     @DisplayName("drawLine 예외 확인")
     @Test
     void 사다리_줄그리기_예외(){
-        //given
-        Ladder ladder = new Ladder(3, 5);
 
         //when
         ladder.drawLine(1,1);
         ladder.drawLine(1,3);
 
-        //줄을 그으려고 하는 사다리 오른쪽에 이미 줄이 그어졌을 때
-        assertThrows(Exception.class, ()->ladder.drawLine(1,2));
-
-        // 사다리 높이가 생성된 사다리 높이보다 높을 때
-        assertThrows(Exception.class, ()->ladder.drawLine(4,2));
-
-        // 이미 줄이 그어진 곳에 줄을 그리려고 할 때
-        assertThrows(Exception.class, ()->ladder.drawLine(1,1));
-
-        // 줄을 그을 수 없는 사다리를 선택했을 때때
-        assertThrows(Exception.class, ()->ladder.drawLine(4,1));
+        assertAll(
+                //줄을 그으려고 하는 사다리 오른쪽에 이미 줄이 그어졌을 때
+                ()->assertThrows(Exception.class, ()->ladder.drawLine(1,2)),
+                // 사다리 높이가 생성된 사다리 높이보다 높을 때
+                ()->assertThrows(Exception.class, ()->ladder.drawLine(4,2)),
+                // 이미 줄이 그어진 곳에 줄을 그리려고 할 때
+                ()->assertThrows(Exception.class, ()->ladder.drawLine(1,1)),
+                // 줄을 그을 수 없는 사다리를 선택했을 때때
+                ()->assertThrows(Exception.class, ()->ladder.drawLine(4,1))
+        );
     }
 
     @DisplayName("run 확인")
     @Test
     public void run_확인(){
-        //given
-        Ladder ladder = new Ladder(3, 5);
 
         //when
         ladder.drawLine(0,0);
