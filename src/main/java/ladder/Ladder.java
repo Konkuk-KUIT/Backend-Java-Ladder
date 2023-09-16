327b293e5522a628c24efdc2c1cb494d7af0cc24
rlaxoqkf <rlaxoqkf96@gmail.com>
2023-09-16 14:55:14 +0900

    Docs: update README.md

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -7,4 +7,29 @@ public class Ladder {
     public Ladder(int numberOfRow, int numberOfPerson) {
         rows = new int[numberOfRow][numberOfPerson];
     }
+
+    public void drawLine(int row, int col){
+        checkDraw(row, col); //선을 그을 수 있는 지 확인
+        rows[row][col] = 1;
+        rows[row][col+1] = -1;
+    }
+
+    public void checkDraw(int row, int col) {
+        if(col<0 || col>=rows[0].length-1) // 움직이려는 사다리의 위치에서 줄을 그을 수 없음
+            throw new IllegalStateException();
+        if(row<0 || row >= rows.length) // 그리려는 사다리의 높이의 위치가 생성되어 있는 사다리 높이보다 높을
+            throw new IllegalStateException();
+        if(this.rows[row][col] != 0) // 현재 위치가 이미 줄이 어디론가 그어져 있음
+            throw new IllegalStateException();
+        if(this.rows[row][col+1]==1) // 오른쪽 사다리에 줄이 그어져 있음
+            throw new IllegalStateException();
+    }
+
+    public int run(int start){
+        for(int i = 0; i < rows.length; i++){
+            start = start + rows[i][start];
+        }
+        return start;
+    }
+
 }
--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -1,11 +1,14 @@
 package ladder;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class LadderTest {
 
+    @DisplayName("사다리 생성")
     @Test
     void 사다리_생성_확인() {
         //given
@@ -17,6 +20,47 @@ class LadderTest {
 
         //then
         assertNotNull(ladder);
+    }
+
+    @DisplayName("drawLine 예외 확인")
+    @Test
+    void 사다리_줄그리기_예외(){
+        //given
+        Ladder ladder = new Ladder(3, 5);
+
+        //when
+        ladder.drawLine(1,1);
+        ladder.drawLine(1,3);
+
+        //줄을 그으려고 하는 사다리 오른쪽에 이미 줄이 그어졌을 때
+        assertThrows(Exception.class, ()->ladder.drawLine(1,2));
+
+        // 사다리 높이가 생성된 사다리 높이보다 높을 때
+        assertThrows(Exception.class, ()->ladder.drawLine(4,2));
+
+        // 이미 줄이 그어진 곳에 줄을 그리려고 할 때
+        assertThrows(Exception.class, ()->ladder.drawLine(1,1));
+
+        // 줄을 그을 수 없는 사다리를 선택했을 때때
+        assertThrows(Exception.class, ()->ladder.drawLine(4,1));
+    }
+
+    @DisplayName("run 확인")
+    @Test
+    public void run_확인(){
+        //given
+        Ladder ladder = new Ladder(3, 5);
+
+        //when
+        ladder.drawLine(0,0);
+        ladder.drawLine(0,2);
+        ladder.drawLine(1,1);
+        ladder.drawLine(2,3);
+
+        //then
+        int answer = ladder.run(1); //사다리는 0~4까지 있음 (5명이 하기 때문에)
+        assertEquals(answer,0);
+
     }
 
 }
