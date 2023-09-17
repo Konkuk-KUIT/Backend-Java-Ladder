6a5ead1b4cde8643429ff40d5eece40a49052176
Kjiw0n <rwd0904@naver.com>
2023-09-18 00:45:05 +0900

    Feat: 사다리 기본 로직 구현

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -7,4 +7,28 @@ public class Ladder {
     public Ladder(int numberOfRow, int numberOfPerson) {
         rows = new int[numberOfRow][numberOfPerson];
     }
+
+    public void drawLine(int row, int col) {
+        rows[row][col] = 1; //오른쪽
+        rows[row][col + 1] = -1; //왼쪽
+        //ㅜㅜ
+    }
+
+    public int run(int position) {
+        for (int i = 0; i < rows.length; i++) {
+            position = nextPosition(position, i);
+        }
+        return position;
+    }
+
+    private int nextPosition(int position, int row) {
+        if (rows[row][position] == 1) {
+            return position + 1;
+        }
+        if (rows[row][position] == -1) {
+            return position - 1;
+        }
+        return position;
+    }
+
 }
--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -2,6 +2,7 @@ package ladder;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class LadderTest {
@@ -18,5 +19,27 @@ class LadderTest {
         //then
         assertNotNull(ladder);
     }
+
+    @Test
+    void 결과_확인() {
+        //given
+        int numberOfRow = 3;
+        int numberOfPerson = 2;
+
+        //when
+        Ladder ladder = new Ladder(numberOfRow, numberOfPerson);
+        ladder.drawLine(1, 0);
+
+
+        //then
+        assertEquals(1, ladder.run(0));
+        assertEquals(0, ladder.run(1));
+    }
+
+    @Test
+    void 범위_오류_확인() {
+        //
+    }
+
 
 }
