cb193b033d4a7fe59022569789f61237034e71d2
beomjun <bzzun0609@github.com>
2023-09-17 16:33:24 +0900

    Week1 Mission

--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -1,22 +1,73 @@
 package ladder;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class LadderTest {
+    Ladder ladder;
+    LadderRun ladderRun;
+
+    @BeforeEach
+    void beforeEach() {
+        ladder = new Ladder(6, 5);
+        ladder.drawLine(0, 0);
+        ladder.drawLine(1, 1);
+        ladder.drawLine(2, 3);
+        ladder.drawLine(4, 0);
+        ladder.drawLine(5, 1);
+        ladder.drawLine(4, 3);
+        ladderRun = new LadderRun(ladder);
+    }
 
     @Test
-    void 사다리_생성_확인() {
+    @DisplayName("사다리 생성 확인")
+    void Ladder() {
         //given
         int numberOfRow = 3;
         int numberOfPerson = 5;
-
         //when
-        Ladder ladder = new Ladder(numberOfRow, numberOfPerson);
-
+        Ladder ladderr = new Ladder(numberOfRow, numberOfPerson);
         //then
-        assertNotNull(ladder);
+        assertEquals(3, ladderr.getColumnL());
+        assertEquals(5, ladderr.getRowL());
     }
 
+    @Test
+    @DisplayName("다리 생성 확인")
+    void drawLine() {
+        //given
+        int posY = 2;
+        int posX = 3;
+        //when
+        ladder.drawLine(posY, posX);
+        //then
+        assertEquals(1, ladder.getRows()[posY][posX]);
+        assertEquals(1, ladder.getRows()[posY][posX + 1]);
+    }
+    @Test
+    @DisplayName("사다리 이용 확인")
+    void run() {
+        //given
+        int cur0 = 0;
+        int cur1 = 1;
+        int cur2 = 2;
+        int cur3 = 3;
+        int cur4 = 4;
+        //when
+        cur0 = ladderRun.run(cur0);
+        cur1 = ladderRun.run(cur1);
+        cur2 = ladderRun.run(cur2);
+        cur3 = ladderRun.run(cur3);
+        cur4 = ladderRun.run(cur4);
+        //then
+        assertEquals(1, cur0);
+        assertEquals(2, cur1);
+        assertEquals(0, cur2);
+        assertEquals(3, cur3);
+        assertEquals(4, cur4);
+
+    }
 }
