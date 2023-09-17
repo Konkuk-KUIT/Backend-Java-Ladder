5abf6a25288eb7fbd03ea5bbf6677beb550ea569
beomjun <bzzun0609@github.com>
2023-09-17 16:33:08 +0900

    Week1 Mission

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -1,10 +1,46 @@
 package ladder;
 
 public class Ladder {
-
     private final int[][] rows;
-
     public Ladder(int numberOfRow, int numberOfPerson) {
         rows = new int[numberOfRow][numberOfPerson];
+    }
+
+    public int[][] getRows() {
+        return rows;
+    }
+
+    public int getRowL() {
+        return rows[0].length;
+    }
+
+    public int getColumnL() {
+        return rows.length;
+    }
+
+    public void drawLine(int posY, int posX) {
+        if (canDraw(posY, posX)) {
+            rows[posY][posX] = 1;
+            rows[posY][posX+1] = 1;
+        }
+    }
+
+    private boolean canDraw(int posY, int posX) {
+        // 1.X나 Y가 범위 밖일 경우
+        if (posY >= rows.length || posX >= rows[0].length - 1) {
+            System.out.println("사다리 밖으로는 다리를 만들 수 없습니다.");
+            return false;
+        }
+        // 2. 이미 있는 경우
+        if (rows[posY][posX] == 1 && rows[posY][posX + 1] == 1) {
+            System.out.println("이미 있는 다리는 만들 수 없습니다.");
+            return false;
+        }
+        // 3. 2개 이상의 다리를 연결할 경우
+        if (rows[posY][posX] == 1) {
+            System.out.println("연속된 다리는 만들 수 없습니다.");
+            return false;
+        }
+        return true;
     }
 }
---
+++ src/main/java/ladder/LadderRun.java
@@ -0,0 +1,47 @@
+package ladder;
+
+public class LadderRun {
+    private final Ladder ladder;
+
+    public LadderRun(Ladder ladder) {
+        this.ladder = ladder;
+    }
+
+    public int run(int position) {
+        int curY = 0;
+        int curX = position;
+        while (curY < ladder.getColumnL()) {
+            curX = whereTo(curY, curX);
+            curY++;
+        }
+        return curX;
+    }
+
+    private int whereTo(int curY, int curX) {
+        //안 이어져 있을 경우 ->
+        if (ladder.getRows()[curY][curX] == 0) {
+            return curX;
+        }
+        //이어진 경우 ->
+        //양 끝에 있을 경우
+        if (curX == 0) {
+            curX += 1;
+            return curX;
+        }
+        if (curX == ladder.getRowL() - 1) {
+            curX -= 1;
+            return curX;
+        }
+        //양 끝이 아닐 경우
+        //한 쪽만 이어져 있을 경우
+        if (ladder.getRows()[curY][curX - 1] == 1) {
+            curX -= 1;
+            return curX;
+        }
+        if (ladder.getRows()[curY][curX + 1] == 1) {
+            curX += 1;
+            return curX;
+        }
+        return curX;
+    }
+}
