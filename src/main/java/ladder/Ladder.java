0e4b461f10f186ce4afc01e283c5798d7ab05ff3
Mouon <ahemsapsldk@gmail.com>
2023-09-18 21:45:07 +0900

    Magic Number -> enum class : [Direction.java]

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -3,25 +3,21 @@ package ladder;
 public class Ladder {
 
     /*좌표값으로 나타내기위해 이차원 배열
-    캡슐화, 외부에서 접근 못하게 private로 선언
-    매직 넘버 사용으로 불분명한 숫자 의미 확실하게*/
+    캡슐화, 외부에서 접근 못하게 private로 선언*/
     static Integer[][] ladder;
-    private static final int EMPTY_COLUMN = 2;
-    private static final int CREATED_COLUMN = 1;
     /*인스턴스 생성
     좌표값에해당하는 배열의 값에 CREATED_COLUMN이 들어있으면 가로선 존재*/
     public Ladder(int numberOfRows, int numberOfColumns) {
         ladder = new Integer[numberOfRows][numberOfColumns];
         for (int i = 0; i < numberOfRows; i++) {
             for (int j = 0; j < numberOfColumns; j++) {
-                ladder[i][j] = EMPTY_COLUMN;
+                ladder[i][j] = Direction.EMPTY_COLUMN.getValue();
             }
         }
     }
     public int getLadder(int row,int column){
         return ladder[row][column];
     }
-
     /*사다리게임 진행 메서드, IllegalArgumentException을 사용하여 적절하지 못한 값을 매서드가 받았을때 강제로 예외발생
     들어온 값에만 예외를 처리했는데 current도 로직에 들어가서 문제될수있으므로 조건문으로 진입 막기*/
     public int run(int selectedColumn) {
@@ -38,13 +34,12 @@ public class Ladder {
         /* 리턴값은 1을 더한 값을 반환하여 실제 게임 상의 열을 반환*/
         return currentColumn + 1;
     }
-
     /*가로선 그리는 함수, 가로선은 오른쪽으로만 그려짐 (draw에서 왼쪽오른쪽 선택해서 그리게 하면 코드 구조가 복잡해진다고 생각)*/
     public void drawLine(int row, int column){
         /*범위 벗어나면 예외 발생*/
         if (!LadderMove.isValidRow(row,column)) {
             throw new IllegalArgumentException("Invalid row or position");
         }
-        ladder[row - 1][column - 1] = CREATED_COLUMN;
+        ladder[row - 1][column - 1] = Direction.CREATED_COLUMN.getValue();
     }
 }
--- src/main/java/ladder/LadderMove.java
+++ src/main/java/ladder/LadderMove.java
@@ -4,17 +4,13 @@ package ladder;
 public class LadderMove extends Ladder{
 
     /*좌표값으로 나타내기위해 이차원 배열
-    캡슐화, 외부에서 접근 못하게 private로 선언
-    매직 넘버 사용으로 불분명한 숫자 의미 확실하게*/
-    private static final int CREATED_COLUMN = 1;
-    private static final int FLAGED_COLUMN = 0;
+    캡슐화, 외부에서 접근 못하게 private로 선언*/
 
     public LadderMove(int numberOfRows, int numberOfColumns) {
         super(numberOfRows, numberOfColumns);
     }
     /*인스턴스 생성
     좌표값에해당하는 배열의 값에 CREATED_COLUMN이 들어있으면 가로선 존재*/
-
     public int getLadder(int row,int column){
         return ladder[row][column];
     }
@@ -28,24 +24,24 @@ public class LadderMove extends Ladder{
     }
     /* 현재 들어있는 값이 1인지를 확인*/
     private static boolean canMoveRight(int row, int column){
-        return column < ladder[0].length && ladder[row][column] == CREATED_COLUMN;
+        return column < ladder[0].length && ladder[row][column] == Direction.CREATED_COLUMN.getValue();
     }
     /* 현재 열 왼쪽에 들어있는 값이 1인지를 확인*/
     private static boolean canMoveLeft(int row, int column){
-        return column >0 && ladder[row][column-1] == CREATED_COLUMN;
+        return column >0 && ladder[row][column-1] == Direction.CREATED_COLUMN.getValue();
     }
     private static boolean canMove(int row, int column){
         return canMoveRight(row,column) || canMoveLeft(row,column);
     }
     /*이동한 가로선 비활성*/
     public static void movedFlag(int row, int column){
-        ladder[row][column]=FLAGED_COLUMN;
+        ladder[row][column]=Direction.FLAGED_COLUMN.getValue();
     }
     public static boolean leftFlag(int row, int column) {
-        return column >0 &&ladder[row][column-1]==FLAGED_COLUMN;
+        return column >0 &&ladder[row][column-1]==Direction.FLAGED_COLUMN.getValue();
     }
     public static boolean rightFlag(int row, int column){
-        return  column < ladder[0].length && ladder[row][column]==FLAGED_COLUMN;
+        return  column < ladder[0].length && ladder[row][column]==Direction.FLAGED_COLUMN.getValue();
     }
 
     public static int moveColumn(int row,int column){
@@ -67,10 +63,10 @@ public class LadderMove extends Ladder{
         /* 오른쪽, 왼쪽 모두 이동할 수 없으면 세로로 이동*/
         if(!canMove(row,column)){
             if(leftFlag(row,column)){
-                ladder[row][column-1]=CREATED_COLUMN;
+                ladder[row][column-1]=Direction.CREATED_COLUMN.getValue();
             }
             if(rightFlag(row,column)){
-                ladder[row][column]=CREATED_COLUMN;
+                ladder[row][column]=Direction.CREATED_COLUMN.getValue();
             }
             row++;
         }
