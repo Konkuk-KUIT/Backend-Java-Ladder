31ffa957c9b2205f4138436cdd240f8f627e3e30
YoonSub Lim <yslim37@naver.com>
2023-09-18 23:26:32 +0900

    Feat: 기본 사다리 기능 및 테스트 코드 구현

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -7,4 +7,99 @@ public class Ladder {
     public Ladder(int numberOfRow, int numberOfPerson) {
         rows = new int[numberOfRow][numberOfPerson];
     }
+
+    /** DrawLine 관련 */
+    public void drawLine(Position position){
+        validateDrawable(position);
+        rows[position.x-1][position.y-1] = 1;
+        rows[position.x-1][position.y] = -1;
+    }
+
+    private void validateDrawable(Position position){
+        // 유효하지 않은 좌표값
+        if(position.x > rows.length || position.x-1 < 0 || position.y > rows[0].length || position.y-1 < 0) {
+            throw new IllegalArgumentException("Wrong Position Value");
+        }
+
+        // 이미 그려져있는 line
+        if(rows[position.x-1][position.y-1] == 1) {
+            throw new IllegalArgumentException("Already Drawn Line");
+        }
+
+        // 연속되는 line 금지 : 이어지는 Position 의 Rows Value 가 not 1
+        if(!(position.y-1 <= rows[position.x-1].length - 2 && rows[position.x-1][position.y-1] != 1)){
+            throw new IllegalArgumentException("Cannot Draw Consecutive Line");
+        }
+    }
+
+    /** Run 관련 */
+    public int run(int lineNumber){
+        validateRunnable(lineNumber);
+        Position currentPosition = new Position(0, lineNumber-1);
+
+        while(checkMovable(currentPosition)){
+            moveToNext(currentPosition);
+        }
+        return currentPosition.y + 1;
+    }
+
+    private void validateRunnable(int lineNumber){
+        if(!(lineNumber >= 0 && lineNumber <= rows[0].length)){
+            throw new IllegalArgumentException("Not Runnable Line");
+        }
+    }
+
+    private boolean checkMovable(Position position){
+        if(position.x == rows.length){
+            return false;
+        }
+        return true;
+    }
+
+    private void moveToNext(Position currentPosition){
+        int moveDirection = rows[currentPosition.x][currentPosition.y];
+
+        if(moveDirection == 0){
+            currentPosition.x += 1;
+            return;
+        }
+        currentPosition.x += 1;
+        currentPosition.y += moveDirection;
+    }
+
+    /** 그 외 */
+    public void printResultLadder(){
+        System.out.print("  ");
+        for(int i=1; i<=rows[0].length; i++){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for(int i=0; i<rows.length; i++){
+            System.out.print(i+1 + " ");
+
+            for(int value : rows[i]){
+                switch(value){
+                    case 0:
+                        System.out.print("| ");
+                        break;
+                    case 1:
+                        System.out.print("|-");
+                        break;
+                    case -1:
+                        System.out.print("| ");
+                        break;
+                    default:
+                        System.out.println(value);
+                        throw new RuntimeException();
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public int getValueOfLadder(int row, int col){
+        return rows[row-1][col-1];
+    }
 }
--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -1,13 +1,13 @@
 package ladder;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class LadderTest {
 
     @Test
-    void 사다리_생성_확인() {
+    void 사다리_생성() {
         //given
         int numberOfRow = 3;
         int numberOfPerson = 5;
@@ -17,6 +17,90 @@ class LadderTest {
 
         //then
         assertNotNull(ladder);
+        ladder.printResultLadder();
     }
 
+    @Test
+    void 사다리_라인_생성(){
+        //given
+        int numberOfRow = 3;
+        int numberOfPerson = 5;
+
+        //when
+        Ladder ladder = new Ladder(numberOfRow, numberOfPerson);
+        ladder.drawLine(new Position(1,1));
+        ladder.drawLine(new Position(2,4));
+        ladder.drawLine(new Position(2,1));
+
+        //then
+        assertEquals(1, ladder.getValueOfLadder(1,1));
+        assertEquals(-1, ladder.getValueOfLadder(1,2));
+
+        assertEquals(1, ladder.getValueOfLadder(2,4));
+        assertEquals(-1, ladder.getValueOfLadder(2,5));
+
+        assertEquals(1, ladder.getValueOfLadder(2,1));
+        assertEquals(-1, ladder.getValueOfLadder(2,2));
+
+        ladder.printResultLadder();
+    }
+
+    @Test
+    void 사다리_라인_오류(){
+        //given
+        int numberOfRow = 3;
+        int numberOfPerson = 5;
+
+        //when
+        Ladder ladder = new Ladder(numberOfRow, numberOfPerson);
+        ladder.drawLine(new Position(1,1));
+
+        //then
+        // Case 1 : 사다리의 끝(5)에서 라인 생성
+        assertThrows(Exception.class, () -> ladder.drawLine(new Position(1, 5)));
+
+        // Case 2 : 범위 벗어난 Position
+        assertThrows(Exception.class, () -> ladder.drawLine(new Position(4,3)));
+        assertThrows(Exception.class, () -> ladder.drawLine(new Position(2,6)));
+
+        // Case 3 : 이미 그려진 row
+        assertThrows(Exception.class, () -> ladder.drawLine(new Position(1,1)));
+    }
+
+    @Test
+    void 사다리_동작_비어있는경우(){
+        //given
+        int numberOfRow = 3;
+        int numberOfPerson = 5;
+
+        //when
+        Ladder ladder = new Ladder(numberOfRow, numberOfPerson);
+
+        //then
+        assertEquals(1, ladder.run(1));
+        assertEquals(2, ladder.run(2));
+        assertEquals(3, ladder.run(3));
+    }
+
+    @Test
+    void 사다리_동작_비어있지않은경우(){
+        //given
+        int numberOfRow = 3;
+        int numberOfPerson = 5;
+
+        //when
+        Ladder ladder = new Ladder(numberOfRow, numberOfPerson);
+        ladder.drawLine(new Position(1,1));
+        ladder.drawLine(new Position(1,3));
+        ladder.drawLine(new Position(2,2));
+        ladder.drawLine(new Position(3,1));
+        ladder.drawLine(new Position(3,4));
+
+        //then
+        assertEquals(3, ladder.run(1));
+        assertEquals(2, ladder.run(2));
+        assertEquals(5, ladder.run(3));
+        assertEquals(1, ladder.run(4));
+        assertEquals(4, ladder.run(5));
+    }
 }
