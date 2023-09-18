27920d170b7c68c27561901fb880ad9af5aab212
woookk <woook27@naver.com>
2023-09-18 15:51:35 +0900

    Refactor: Delegate Ladder responsibilities to LadderCreator and LadderRunner

--- src/main/java/ladder/creator/LadderCreator.java
+++ src/main/java/ladder/creator/LadderCreator.java
@@ -1,26 +1,26 @@
-package ladder;
+package ladder.creator;
 
-public class Ladder {
+import ladder.NaturalNumber;
+import ladder.Position;
+import ladder.Row;
 
+public class LadderCreator {
 
     private final Row[] rows;
 
-
-    public Ladder(NaturalNumber numberOfRow, NaturalNumber numberOfPerson) {
+    public LadderCreator(NaturalNumber numberOfRow, NaturalNumber numberOfPerson) {
         rows = new Row[numberOfRow.getNumber()];
         for (int i = 0; i < numberOfRow.getNumber(); i++) {
             rows[i] = new Row(numberOfPerson);
         }
     }
 
+
     public void drawLine(Position row, Position col) {
         rows[row.getValue()].drawLine(col);
     }
 
-    public int run(Position position) {
-        for (int i = 0; i < rows.length; i++) {
-            position = rows[i].nextPosition(position);
-        }
-        return position.getValue();
+    public Row[] getRows() {
+        return rows;
     }
 }
--- src/test/java/ladder/LadderGameTest.java
+++ src/test/java/ladder/LadderGameTest.java
@@ -1,10 +1,11 @@
 package ladder;
 
+import ladder.creator.LadderCreator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class LadderTest {
+class LadderGameTest {
 
     @Test
     void 사다리_생성_확인() {
@@ -13,10 +14,10 @@ class LadderTest {
         NaturalNumber numberOfPerson = NaturalNumber.of(5);
 
         //when
-        Ladder ladder = new Ladder(numberOfRow, numberOfPerson);
+        LadderCreator ladderCreator = new LadderCreator(numberOfRow, numberOfPerson);
 
         //then
-        assertNotNull(ladder);
+        assertNotNull(ladderCreator);
     }
 
     @Test
@@ -24,14 +25,15 @@ class LadderTest {
         //when
         NaturalNumber numberOfRow = NaturalNumber.of(1);
         NaturalNumber numberOfPerson = NaturalNumber.of(3);
-        Ladder ladder = new Ladder(numberOfRow, numberOfPerson);
+        LadderCreator ladderCreator = new LadderCreator(numberOfRow, numberOfPerson);
+        LadderGame ladderGame = new LadderGame(ladderCreator);
 
         //given
         int nthOfPerson = 4;
         Position position = Position.of(nthOfPerson);
 
         //then
-        assertThrows(IllegalArgumentException.class, ()->ladder.run(position));
+        assertThrows(IllegalArgumentException.class, ()->ladderGame.run(position));
     }
 
     @Test
@@ -39,32 +41,34 @@ class LadderTest {
         //when
         NaturalNumber numberOfRow = NaturalNumber.of(3);
         NaturalNumber numberOfPerson = NaturalNumber.of(4);
-        Ladder ladder = new Ladder(numberOfRow, numberOfPerson);
+        LadderCreator ladderCreator = new LadderCreator(numberOfRow, numberOfPerson);
 
-        ladder.drawLine(Position.of(0),Position.of(0));
-        ladder.drawLine(Position.of(1),Position.of(1));
-        ladder.drawLine(Position.of(2),Position.of(0));
+        LadderGame ladderGame = new LadderGame(ladderCreator);
+
+        ladderCreator.drawLine(Position.of(0),Position.of(0));
+        ladderCreator.drawLine(Position.of(1),Position.of(1));
+        ladderCreator.drawLine(Position.of(2),Position.of(0));
 
         //given
         int nthOfPerson = 0;
         Position position = Position.of(nthOfPerson);
 
         //then
-        assertEquals(2, ladder.run(position));
+        assertEquals(2, ladderGame.run(position));
 
         //given
         nthOfPerson = 1;
         position = Position.of(nthOfPerson);
 
         //then
-        assertEquals(1, ladder.run(position));
+        assertEquals(1, ladderGame.run(position));
 
         //given
         nthOfPerson = 2;
         position = Position.of(nthOfPerson);
 
         //then
-        assertEquals(0, ladder.run(position));
+        assertEquals(0, ladderGame.run(position));
     }
 
 }
