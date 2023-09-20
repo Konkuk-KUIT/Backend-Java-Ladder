f963bd2590fbebede4e46282b4ed710067b6666f
beomjun <bzzun0609@github.com>
2023-09-21 01:02:06 +0900

    refactor: Using ExceptionMessage.java

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -21,18 +21,17 @@ public class Ladder {
     public void drawLine(int posY, int posX) {
         // 1.X나 Y가 범위 밖일 경우
         if (posY >= rows.length || posX >= rows[0].length - 1) {
-            throw new ArrayIndexOutOfBoundsException("사다리 밖으로는 다리를 만들 수 없습니다.");
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_DRAW_POSITION_OUT.getMessage());
         }
         // 2. 이미 있는 경우
         if (rows[posY][posX] == 1 && rows[posY][posX + 1] == 1) {
-            throw new RuntimeException("이미 있는 다리는 만들 수 없습니다.");
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_DRAW_POSITION_EXIST.getMessage());
         }
         // 3. 2개 이상의 다리를 연결할 경우
         if (rows[posY][posX] == 1) {
-            throw new IllegalStateException("두 개 이상의 다리는 연결할 수 없습니다.");
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_DRAW_POSITION_OVERLAP.getMessage());
         }
         rows[posY][posX] = 1;
         rows[posY][posX+1] = 1;
     }
-
 }
