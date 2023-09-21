e74a97aa41f66cf81d9a12de7bbd2c44e8136569
beomjun <bzzun0609@github.com>
2023-09-21 15:42:54 +0900

    refactor: LadderRunner.java
    Using seperated responsibility

--- src/main/java/ladder/LadderRunner.java
+++ src/main/java/ladder/LadderRunner.java
@@ -1,47 +1,17 @@
 package ladder;
 
 public class LadderRunner {
-    private final Ladder ladder;
 
-    public LadderRunner(Ladder ladder) {
-        this.ladder = ladder;
+    private final Row[] rows;
+
+    public LadderRunner(Row[] rows) {
+        this.rows = rows;
     }
 
-    public int run(int position) {
-        int curY = 0;
-        int curX = position;
-        while (curY < ladder.getColumnL()) {
-            curX = whereTo(curY, curX);
-            curY++;
+    public int run(Position position) {
+        for (int i = 0; i < rows.length; i++) {
+            position = rows[i].nextPosition(position);
         }
-        return curX;
-    }
-
-    private int whereTo(int curY, int curX) {
-        //안 이어져 있을 경우 ->
-        if (ladder.getRows()[curY][curX] == 0) {
-            return curX;
-        }
-        //이어진 경우 ->
-        //양 끝에 있을 경우
-        if (curX == 0) {
-            curX += 1;
-            return curX;
-        }
-        if (curX == ladder.getRowL() - 1) {
-            curX -= 1;
-            return curX;
-        }
-        //양 끝이 아닐 경우
-        //한 쪽만 이어져 있을 경우
-        if (ladder.getRows()[curY][curX - 1] == 1) {
-            curX -= 1;
-            return curX;
-        }
-        if (ladder.getRows()[curY][curX + 1] == 1) {
-            curX += 1;
-            return curX;
-        }
-        return curX;
+        return position.getPosition();
     }
 }
