780bad15fa9f7733c31ba860ae3d0587e0692f6d
Mouon <ahemsapsldk@gmail.com>
2023-09-22 11:18:56 +0900

    LadderCreator 기능 삭제

--- src/main/java/ladder/LadderCreator.java
+++ src/main/java/ladder/LadderCreator.java
@@ -1,29 +1,5 @@
 package ladder;
 
-import java.util.Random;
-
 public class LadderCreator {
 
-    private final Integer[] ladders;
-
-    public LadderCreator(NaturalNumber numberOfRow, NaturalNumber numberOfPerson) {
-        ladders = new Integer[numberOfRow.getNumber()];
-        Random random = new Random();
-
-        for (int i = 0; i < numberOfRow.getNumber(); i++) {
-            int randomColumn = random.nextInt(numberOfPerson.getNumber());
-
-            // Check if the generated column is consecutive to the previous one
-            if (i > 0 && Math.abs(ladders[i - 1] - randomColumn) == 1) {
-                // If consecutive, generate a new random column
-                randomColumn = (randomColumn + 1) % numberOfPerson.getNumber();
-            }
-
-            ladders[i] = new Integer(randomColumn);
-        }
-    }
-
-    public Integer[] getRows() {
-        return ladders;
-    }
 }
