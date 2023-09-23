ff6e94a421dd1342787ab951f0a468b1dbf44293
beomjun <bzzun0609@github.com>
2023-09-23 14:28:05 +0900

    refactor: Applying factoryMethod pattern

--- src/main/java/ladder/LadderCreator.java
+++ src/main/java/ladder/LadderCreator.java
@@ -1,20 +1,5 @@
 package ladder;
 
-public class LadderCreator {
-    private final Row[] rows;
+public interface LadderCreator {
 
-    public LadderCreator(NaturalNumber numberOfRow, NaturalNumber numberOfPerson) {
-        rows = new Row[numberOfRow.getNumber()];
-        for (int i = 0; i < numberOfRow.getNumber(); i++) {
-            rows[i] = new Row(numberOfPerson);
-        }
-    }
-
-    public void drawLine(Position row, Position col) {
-        rows[row.getPosition()].drawLine(col);
-    }
-
-    public Row[] getRows() {
-        return rows;
-    }
 }
