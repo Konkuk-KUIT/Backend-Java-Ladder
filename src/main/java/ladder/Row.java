4c58e522a93683a46fb76165e7244a1f538ea454
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2023-09-24 17:13:54 +0900

    Refactor: ExceptionMessage enum for centralized error messages

--- src/main/java/ladder/Row.java
+++ src/main/java/ladder/Row.java
@@ -1,5 +1,8 @@
 package ladder;
 
+
+import static ladder.ExceptionMessage.*;
+
 public class Row {
     int[] row;
 
@@ -36,19 +39,19 @@ public class Row {
 
     private void validateNumberOfPerson(int numberOfPerson) {
         if (numberOfPerson < 1) {
-            throw new IllegalArgumentException("참여 인원은 1명 이상이어야 합니다.");
+            throw new IllegalArgumentException(INVALID_NUMBER_OF_PERSON.getMessage());
         }
     }
 
     private void validatePosition(int position) {
         if (position >= row.length || position < 0) {
-            throw new IllegalArgumentException("유효하지 않은 위치입니다.");
+            throw new IllegalArgumentException(INVALID_POSITION.getMessage());
         }
     }
 
     private void validateDrawLinePosition(int startPosition) {
         if (startPosition >= row.length - 1 || startPosition < 0 || row[startPosition] == Direction.LEFT.getValue() || row[startPosition + 1] == Direction.RIGHT.getValue()) {
-            throw new IllegalArgumentException("사다리를 그릴 수 없는 위치입니다.");
+            throw new IllegalArgumentException(INVALID_DRAW_POSITION.getMessage());
         }
     }
 }
